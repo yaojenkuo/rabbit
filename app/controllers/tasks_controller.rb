045e1d702d2337c32797843662ca1cce0668b922
yaojenkuo <tonykuoyj@gmail.com>
2017-06-06 23:18:03 +0800

    added crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,9 +25,17 @@ class TasksController < ApplicationController
     end
 
     def update
+        if @task.update(tasks_params)
+            redirect_to @task
+        else
+            render 'Edit'
+        end
+        
     end
     
     def destroy
+        @task.destroy
+        redirect_to root_path
     end
     
     private
