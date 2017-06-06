b10c3e8d4c47799830966ac275027df6137db2b5
yaojenkuo <tonykuoyj@gmail.com>
2017-06-06 18:28:48 +0800

    added index and show actions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+    before_action :find_task, except: [:index, :create, :new]
+    
+    def index
+        @tasks = Task.all.order("created_at DESC")
+    end
+    
+    def show
+    end
+    
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(tasks_params)
+        if @task.save
+            redirect_to @task
+        else
+            render 'New'
+        end
+    end
+    
+    def edit
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
+    
+    private
+    def tasks_params
+        params.require(:task).permit(:title, :description, :company, :url)
+    end
+
+    def find_task
+        @task = Task.find(params[:id])
+    end
+    
+end
